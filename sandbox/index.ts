af54a5392e68cd3dc995c9929332f177c5f85f4a
praveenkumar09 <tpkprav@gmail.com>
2024-02-28 17:15:45 +0800

    Initial Commit

---
+++ sandbox/index.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config';
+import { client } from '../src/services/redis';
+
+const run = async () => {
+    await client.hSet('car',{
+        color : 'red',
+        year:1950,
+        engine : {
+            cylinders:8
+        },
+        owner : null || '',
+        service:undefined || ''
+    })
+
+    const car = await client.hGetAll('car');
+    console.log(car);
+};
+run();
