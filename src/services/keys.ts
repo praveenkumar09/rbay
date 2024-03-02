1165954cc0874db33a02e4a442a7327aa95c5c55
praveenkumar09 <tpkprav@gmail.com>
2024-03-03 07:58:32 +0800

    Added in likes functionality

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,3 +1,6 @@
 export const pageCacheKey = (id : string) => `pagecache#${id}`;
 export const usersKey = (userId: string) => `users#${userId}`;
 export const sessionsKey = (sessionId: string) => `sessions#${sessionId}`;
+export const itemsKey = (itemId: string) => `items#${itemId}`;
+export const usernamesUniqueKey = () => 'useernames:unique';
+export const userLikesKey = (userId: string) => `users:likes#${userId}`;
