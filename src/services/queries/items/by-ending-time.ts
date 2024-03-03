e39171c4315139b150522a8836ce8bdfae7ef0f9
praveenkumar09 <tpkprav@gmail.com>
2024-03-04 06:27:10 +0800

    Fixed ending soonest and arrange by views

--- src/services/queries/items/by-ending-time.ts
+++ src/services/queries/items/by-ending-time.ts
@@ -1,5 +1,23 @@
+import { itemsByEndingSoonestKey, itemsKey } from "$services/keys";
+import { client } from "$services/redis";
+import { deserialize } from "./deserialize";
+import { getItems } from "./items";
+
 export const itemsByEndingTime = async (
 	order: 'DESC' | 'ASC' = 'DESC',
 	offset = 0,
 	count = 10
-) => {};
+) => {
+	const ids = await client.zRange(
+		itemsByEndingSoonestKey(),
+		Date.now(),
+		'+inf',
+		{
+			BY : 'SCORE',
+			LIMIT : {
+				offset,
+				count
+			}
+		});
+		return await getItems(ids);
+};
--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -2,7 +2,7 @@ import type { CreateItemAttrs } from '$services/types';
 import { client } from '$services/redis';
 import { serialize } from './serialize';
 import { genId } from '$services/utils';
-import { itemsKey } from '$services/keys';
+import { itemsKey,itemsByViewsKey, itemsByEndingSoonestKey} from '$services/keys';
 import { deserialize } from './deserialize';
 
 export const getItem = async (id: string) => {
@@ -29,6 +29,16 @@ export const getItems = async (ids: string[]) => {
 export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
     const id = genId();
     const serialized = serialize(attrs);
-    await client.hSet(itemsKey(id),serialized);
+    await Promise.all([
+        client.hSet(itemsKey(id),serialized),
+        client.zAdd(itemsByViewsKey(),{
+            value:id,
+            score:0
+        }),
+        client.zAdd(itemsByEndingSoonestKey(),{
+            value:id,
+            score:attrs.endingAt.toMillis()
+        })
+    ]);
     return id;
 };
