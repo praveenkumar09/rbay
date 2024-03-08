1a98cc76728f851967a5574af37426aa7e041b2e
praveenkumar09 <tpkprav@gmail.com>
2024-03-08 19:32:34 +0800

    Items By view functionality

--- src/services/queries/items/by-views.ts
+++ src/services/queries/items/by-views.ts
@@ -1,8 +1,9 @@
 import { itemsByViewsKey, itemsKey } from "$services/keys";
 import { client } from "$services/redis";
+import { deserialize } from "./deserialize";
 
 export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
-    const results = await client.sort(
+    let results: any = await client.sort(
         itemsByViewsKey(),
         {
             GET:[
@@ -18,5 +19,14 @@ export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, c
             }
         }
     );
+
+    const items = [];
+    while(results.length) {
+        const [id, name, views, ...rest] = results;  
+        const item  =deserialize(id, {name, views});
+        items.push(item);
+        results = rest;
+    }
     console.log(results);
+    return items;
 };
