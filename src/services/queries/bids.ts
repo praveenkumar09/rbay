79f49cf4fae3ccc38157c1c9c23b7aaca1e5c406
praveenkumar09 <tpkprav@gmail.com>
2024-03-10 18:26:52 +0800

    Bid updates in the item object added

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -1,11 +1,29 @@
 import type { CreateBidAttrs, Bid } from '$services/types';
-import { bidHistoryKey } from '$services/keys';
+import { bidHistoryKey, itemsKey } from '$services/keys';
 import { client } from '$services/redis';
 import { DateTime } from 'luxon';
+import { getItem } from './items';
 
 export const createBid = async (attrs: CreateBidAttrs) => {
+	const item = await getItem(attrs.itemId);
+	if(!item){
+		throw new Error("Item does not exist");
+	}
+	if(item.price >= attrs.amount){
+		throw new Error("Bid too low");
+	}
+	if(item.endingAt.diff(DateTime.now()).toMillis() < 0){
+		throw new Error("Item closed to bidding");
+	}
 	const serlializedData = serializeHistory(attrs.amount,attrs.createdAt.toMillis());
-	return client.rPush(bidHistoryKey(attrs.itemId),serlializedData);
+	Promise.all([
+		client.rPush(bidHistoryKey(attrs.itemId),serlializedData),
+		client.hSet(itemsKey(item.id),{
+			bids: item.bids + 1,
+			price : attrs.amount,
+			highestBidUserId : attrs.userId
+		})
+	]);
 };
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
