4a0d3cdda66934ec224d24008ad7fd59511b1b62
praveenkumar09 <tpkprav@gmail.com>
2024-03-13 07:06:20 +0800

    Added lua script in client.js

--- src/services/redis/client.ts
+++ src/services/redis/client.ts
@@ -1,11 +1,25 @@
-import { createClient } from 'redis';
+import { createClient,defineScript } from 'redis';
 
 const client = createClient({
 	socket: {
 		host: process.env.REDIS_HOST,
 		port: parseInt(process.env.REDIS_PORT)
 	},
-	password: process.env.REDIS_PW
+	password: process.env.REDIS_PW,
+	scripts : {
+		addOneAndStore: defineScript({
+			NUMBER_OF_KEYS:1,
+			SCRIPT: `
+			  return redis.call('SET',KEYS[1],1 + tonumber(ARGV[1]))
+			`,
+			transformArguments(key: string,value: number){
+				return [key, value.toString()]
+			},
+			transformReply(reply: any){
+				return reply
+			}
+		})
+	}
 });
 
 client.on('error', (err) => console.error(err));
