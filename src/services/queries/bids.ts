88642b9b89e4d2936609f33d2d5a25b9f7f0bfd9
praveenkumar09 <tpkprav@gmail.com>
2024-03-12 08:17:37 +0800

    Isolated transactions with Redis for Bids

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -5,25 +5,30 @@ import { DateTime } from 'luxon';
 import { getItem } from './items';
 
 export const createBid = async (attrs: CreateBidAttrs) => {
-	const item = await getItem(attrs.itemId);
-	if(!item){
-		throw new Error("Item does not exist");
-	}
-	if(item.price >= attrs.amount){
-		throw new Error("Bid too low");
-	}
-	if(item.endingAt.diff(DateTime.now()).toMillis() < 0){
-		throw new Error("Item closed to bidding");
-	}
-	const serlializedData = serializeHistory(attrs.amount,attrs.createdAt.toMillis());
-	Promise.all([
-		client.rPush(bidHistoryKey(attrs.itemId),serlializedData),
-		client.hSet(itemsKey(item.id),{
+
+	return client.executeIsolated(async (isolatedClient) => {
+		await isolatedClient.watch(itemsKey(attrs.itemId));
+		const item = await getItem(attrs.itemId);
+	    if(!item){
+		  throw new Error("Item does not exist");
+	    }
+	    if(item.price >= attrs.amount){
+		  throw new Error("Bid too low");
+	    }
+	    if(item.endingAt.diff(DateTime.now()).toMillis() < 0){
+		  throw new Error("Item closed to bidding");
+	    }
+	    const serlializedData = serializeHistory(attrs.amount,attrs.createdAt.toMillis());
+	    return isolatedClient
+		  .multi()
+		  .rPush(bidHistoryKey(attrs.itemId),serlializedData)
+		  .hSet(itemsKey(item.id),{
 			bids: item.bids + 1,
 			price : attrs.amount,
 			highestBidUserId : attrs.userId
-		})
-	]);
+		   })
+		   .exec();
+	});	
 };
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
