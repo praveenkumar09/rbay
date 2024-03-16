834171f125271691fe58274bdea2ec0bfbc9a7f4
praveenkumar09 <tpkprav@gmail.com>
2024-03-16 08:50:13 +0800

    Fixed item add fn

--- src/services/redis/lock.ts
+++ src/services/redis/lock.ts
@@ -14,7 +14,8 @@ export const withLock = async (key: string,cb : () => any) => {
 		retries --;
 	   //Try to do a SET NX operation
 	   const acquired = await client.set(lockKey,token,{
-	   NX:true
+	   NX:true,
+	   PX:2000
 	});
 	if(!acquired){
 	   // else brief pause and then retry
